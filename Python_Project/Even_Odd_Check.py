b32479b999ded5b7726196c43436df9b795a9197
Rakesh Kumar Jha <81734953+rakesh611@users.noreply.github.com>
2026-02-08 12:01:45 +0530

    Update Even_Odd_Check.py

--- Python_Project/Even_Odd_Check.py
+++ Python_Project/Even_Odd_Check.py
@@ -1,6 +1,8 @@
+python```
 Number = int(input("please enter a number:\n"))
 Number_to_check = Number % 2
 if Number_to_check == 0:
     print("number is even")
 else:
     print("number is odd")
+```
