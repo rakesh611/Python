a0fb548e20c9428d95d1575a1417ccc0ad7d6aa7
rakesh611 <jharakesh485@gmail.com>
2026-09-21 09:32:19 +0530

    string-strip

--- Day_02/examples/01-string-strip.py
+++ Day_02/examples/01-string-strip.py
@@ -1,3 +1,27 @@
+# strip()
+# Removes spaces from both sides:
+# Example:
 text = "   Some spaces around   "
 stripped_text = text.strip()
+print("Stripped text:", stripped_text)
+################################
+# lstrip()
+# Removes from left:
+# Example:
+text = "   Some spaces around   "
+left_stripped_text = text.lstrip()
+print("Left stripped text:", left_stripped_text)
+# Output = Left stripped text: Some spaces around
+################################
+# rstrip()
+# Removes from right:
+# Example:
+text = "   Some spaces around   "
+right_stripped_text = text.rstrip()
+print("Right stripped text:", right_stripped_text)
+# output = Right stripped text:    Some spaces around
+##############################
+# We can also remove specific characters:
+text = "###Some spaces around###"
+stripped_text = text.strip("#")
 print("Stripped text:", stripped_text)
