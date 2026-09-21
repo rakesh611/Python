c44cb5a9f478a0d64a870553fc1f34a878949a40
rakesh611 <jharakesh485@gmail.com>
2026-09-21 09:06:54 +0530

    3 rd example of split

--- Day_02/examples/01-string-split.py
+++ Day_02/examples/01-string-split.py
@@ -11,3 +11,9 @@ length = len(words[0])
 print("Length of the words:", length)
 uppercase = words[0].upper()
 print("Uppercase of the words:", uppercase)
+
+# 3rd example
+python = "Python is a high-level programming language"
+words = python.split()
+new_words = words[3].split("-")
+print("words:", words[0], words[1], words[2], new_words[0], new_words[1], words[4], words[5])
