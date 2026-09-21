c673fa5fb9abe346f48f5e1148c4f861b157ac6a
rakesh611 <jharakesh485@gmail.com>
2026-09-21 09:17:39 +0530

    rsplit

--- Day_02/examples/01-string-split.py
+++ Day_02/examples/01-string-split.py
@@ -12,8 +12,15 @@ print("Length of the words:", length)
 uppercase = words[0].upper()
 print("Uppercase of the words:", uppercase)
 
-# 3rd example
+# 3. split("-") — Split using -
 python = "Python is a high-level programming language"
 words = python.split()
 new_words = words[3].split("-")
 print("words:", words[0], words[1], words[2], new_words[0], new_words[1], words[4], words[5])
+
+# 4. rsplit() — Split from the right side
+language = "Python is a high-level programming language"
+new_language = language.rsplit(" ", 1)
+new_language1 = language.rsplit(" ", 2)
+print("new_language:", new_language)
+print("new_language1:", new_language1)
